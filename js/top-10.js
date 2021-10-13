904b75e0158c1fc9d759004b3103be76a3cce615
acarnevali <amcarnevali92@gmail.com>
2021-10-13 14:25:46 -0300

    Implemented JS and Implemented background image

---
+++ js/top-10.js
@@ -0,0 +1,17 @@
+let listaFilmes = [
+    'https://br.web.img3.acsta.net/pictures/21/07/07/00/23/1802515.jpg',
+    'https://imagens.publicocdn.com/imagens.aspx/765168?tp=KM',
+    'https://br.web.img3.acsta.net/pictures/14/10/31/20/39/476171.jpg',
+    'https://br.web.img2.acsta.net/pictures/21/08/16/10/26/0653161.jpg',
+    'https://upload.wikimedia.org/wikipedia/pt/f/f1/A_Menina_que_Matou_os_Pais.JPG',
+    'https://upload.wikimedia.org/wikipedia/pt/b/b7/Venom_2018.jpg',
+    'https://i.ytimg.com/vi/T9jIcIwuU7M/movieposter_en.jpg',
+    'https://br.web.img3.acsta.net/pictures/21/06/22/22/19/1495362.jpg',
+    'https://br.web.img3.acsta.net/pictures/21/07/23/21/46/5924644.jpg',
+    'https://t.ctcdn.com.br/l6o8XKNHHVHboZsHMUI6Jyz5PcI=/1024x0/smart/i495367.jpeg',
+]
+
+
+    for (let filme of listaFilmes) {
+        document.write('<img src=' + filme + '>')
+    }
